d41c825bb05cb4dfd87a9ef0285fe2f717040e57
unknown <prakashthakur0021@gmail.com>
2021-01-20 03:31:23 +0530

    nodemon and concurrently dev dependency setup

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,15 @@ const express = require('express')
 const products = require('./data/products')
 const app = express();
 
-
-
 app.get('/', (req,res) => {
     res.send('app is running....')
 })
 
-app.get('/api/product', (req,res) => {
+app.get('/api/products', (req,res) => {
     res.json(products)
 })
 
-app.get('/api/product/:id', (req,res) => {
+app.get('/api/products/:id', (req,res) => {
     const product = products.find(p => p._id === req.params.id)
     res.json(product)
 })
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
 
 
 const ProductScreen = ({match}) => {
+    const [product, setProduct] = useState([]);
 
-    const product = products.find(p => p._id === match.params.id)
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const {data} = await axios.get(`/api/products/${match.params.id}`)
+            setProduct(data)
+        }
+        fetchProducts();
+
+    }, [match])
 
     return (
         <>
