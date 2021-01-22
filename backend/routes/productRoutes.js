e14ec4cd63203453ae0aa4eff4a20f020906f18f
unknown <prakashthakur0021@gmail.com>
2021-01-22 18:51:56 +0530

    middleware added

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -9,12 +9,7 @@ import asyncHandler from 'express-async-handler'
 router.get('/', asyncHandler(async (req,res) => {
     const products = await Product.find({})
 
-    if(products) {
-        res.json(products)
-    } else {
-        console.log('problem')
-        res.status(404).json({ message: 'Product not found'})
-    }
+    res.json(products)
 }))
 
 
@@ -24,13 +19,12 @@ router.get('/', asyncHandler(async (req,res) => {
 router.get('/:id', asyncHandler(async (req,res) => {
 
     const product = await Product.findById(req.params.id)
-    console.log(product);
+    
     if(product) {
-        console.log('kamlehs')
         res.json(product)
     } else {
-        console.log('problem')
-        res.status(404).json({ message: 'Product not found'})
+        res.status(404)
+        throw new Error('Product not found')
     }
 
 }))
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import  express  from 'express'
 import dotenv  from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
+import { notFound, errorHandler} from './midddleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
 const app = express();
 
@@ -14,6 +15,11 @@ app.get('/', (req,res) => {
 })
 
 app.use('/api/products',productRoutes);
+
+app.use(notFound)
+
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT || 5000
 
