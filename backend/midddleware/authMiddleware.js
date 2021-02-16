8681efde4690bb4e08b47542734f5587dd74bec4
unknown <prakashthakur0021@gmail.com>
2021-02-16 18:48:29 +0530

    admin user page

--- backend/midddleware/authMiddleware.js
+++ backend/midddleware/authMiddleware.js
@@ -29,4 +29,15 @@ const protect = asyncHandler(async (req, res, next) => {
 
 })
 
-export { protect}
+const admin = (req, res, next) => {
+    if (req.user || req.user.isAdmin === true ) {
+        next()
+    } else {
+        res.status(401)
+        throw new Error('Not authorized as an admin')
+    }
+
+}
+
+
+export { protect, admin}
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,7 +3,7 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools} from 'redux-devtools-extension'
 import { productReducers, productDetailsReducers } from './reducers/productReducers'
 import {cartReducers } from './reducers/cartReducers'
-import { userDetailsReducer , userLoginReducer, userRegisterReducer, userUpdateProfileReducer } from './reducers/userReducers'
+import { userDeleteReducer, userDetailsReducer , userListReducer, userLoginReducer, userRegisterReducer, userUpdateProfileReducer, userUpdateReducer } from './reducers/userReducers'
 import { orderCreateReducer, orderDetailsReducer, orderPayReducer, orderListMyReducer } from './reducers/orderReducers'
 
 const reducers = combineReducers({
@@ -14,11 +14,13 @@ const reducers = combineReducers({
     userRegister : userRegisterReducer,
     userDetails : userDetailsReducer,
     userUpdateProfile : userUpdateProfileReducer,
+    userList : userListReducer,
+    userDelete : userDeleteReducer,
+    userUpdate : userUpdateReducer,
     orderCreate: orderCreateReducer,
     orderDetails: orderDetailsReducer,
     orderPay: orderPayReducer,
     orderListMy: orderListMyReducer,
-
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
