8d9cb29f14fdd0a3a1570654a4b8f4f00cf998ef
unknown <prakashthakur0021@gmail.com>
2021-02-24 19:28:48 +0530

    prepare for deployment

--- frontend/src/components/Meta.js
+++ frontend/src/components/Meta.js
@@ -12,7 +12,7 @@ const Meta = ({ title, description, keywords}) => {
 }
 
 Meta.defaultProps = {
-    title: 'Welcome to Proshop',
+    title: 'ShoppingComplex',
     description: 'We sell the best products for cheap and best qualtiy',
     keywords: 'electronics, buy electronics, cheap electronics, latest electronics'
 }
